b28610ef819bc581efa2ed50ff33faeb188b5aec
Eric Carvalho <ericcarvalho935@gmail.com>
2025-01-04 00:29:05 -0300

    feat: constructor jogo

--- src/main/kotlin/br/com/gamesinfo/modelo/Jogo.kt
+++ src/main/kotlin/br/com/gamesinfo/modelo/Jogo.kt
@@ -2,13 +2,27 @@ package br.com.gamesinfo.modelo// Val -> somente leitura, recebem valore apenas 
 // Var -> !val
 
 data class Jogo(
-    val titulo: String,
-    val capa: String,
-    val preco: Double,
-    var descricao: String,
-    val id: Int
+    var titulo: String,
+    var capa: String,
+    var preco: Double,
+    var descricao: String
 ) {
+    private var id:Int? = null
+
     override fun toString(): String {
-        return "\ntitulo: $titulo\ncapa: $capa\ndescricao: $descricao\n"
+        return "\nid: $id\ntitulo: $titulo\ncapa: $capa\npreço: $preco\ndescricao: $descricao\n"
+    }
+
+    constructor(
+        titulo: String,
+        capa: String,
+        preco: Double,
+        descricao: String,
+        id: Int
+    ) : this(titulo, capa, preco, descricao) {
+        this.descricao = descricao
+        this.capa = capa
+        this.preco = preco
+        this.id = id
     }
 }
--- src/main/kotlin/br/com/gamesinfo/principal/TesteBanco.kt
+++ src/main/kotlin/br/com/gamesinfo/principal/TesteBanco.kt
@@ -6,4 +6,6 @@ import br.com.gamesinfo.modelo.Jogo
 fun main() {
     val listaJogos: List<Jogo> = Banco.getJogos()
     println(listaJogos)
+
+    val Jogo = Jogo("Teste Inclusao", "Teste Inclusao", 2.7, "Teste")
 }
